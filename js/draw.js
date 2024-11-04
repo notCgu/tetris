5e155a168593ca2758cf25511de84b46849134b1
not etienne <166166484+notCgu@users.noreply.github.com>
2024-11-04 15:27:42 -0800

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -15,51 +15,87 @@ function drawPieceQueue(){
 
 function drawPieceQueueBlock(x, y, type){
   const head = document.getElementById("head");
+  pieceQueueDivs.push([document.createElement("div"),document.createElement("div"),document.createElement("div"),document.createElement("div")]);
   switch(type){
     case 1:
-      pieceQueueDivs.push([]);
-      rect(x-40, y, 20, 20);
-      rect(x-20, y, 20, 20);
-      rect(x, y, 20, 20);
-      rect(x+20, y, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 260;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 280;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 300;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 320;
+      for(let i = 0; i < 4; i ++){
+        pieceQueueDivs[pieceQueueDivs.length-1][i].style.marginTop = 360;
+      }
       break;
     case 2:
-      rect(x-30, y-10, 20, 20);
-      rect(x-30, y+10, 20, 20);
-      rect(x-10, y+10, 20, 20);
-      rect(x+10, y+10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 310;
       break;
     case 3:
-      rect(x-30, y+10, 20, 20);
-      rect(x-10, y+10, 20, 20);
-      rect(x+10, y+10, 20, 20);
-      rect(x+10, y-10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 290;
       break;
     case 4:
-      rect(x-20, y-10, 20, 20);
-      rect(x-20, y+10, 20, 20);
-      rect(x, y-10, 20, 20);
-      rect(x, y+10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 280;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 280;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 300;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 300;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 310;
       break;
     case 5:
-      rect(x-30, y+10, 20, 20);
-      rect(x-10, y+10, 20, 20);
-      rect(x-10, y-10, 20, 20);
-      rect(x+10, y-10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 290;
       break;
     case 6:
-      rect(x-30, y+10, 20, 20);
-      rect(x-10, y+10, 20, 20);
-      rect(x-10, y-10, 20, 20);
-      rect(x+10, y+10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 310;
       break;
     case 7:
-      rect(x-30, y-10, 20, 20);
-      rect(x-10, y-10, 20, 20);
-      rect(x-10, y+10, 20, 20);
-      rect(x+10, y+10, 20, 20);
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginLeft = 270;
+      pieceQueueDivs[pieceQueueDivs.length-1][0].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][1].style.marginTop = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginLeft = 290;
+      pieceQueueDivs[pieceQueueDivs.length-1][2].style.marginTop = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginLeft = 310;
+      pieceQueueDivs[pieceQueueDivs.length-1][3].style.marginTop = 310;
       break;
   }
+  for(let i = 0; i < 4; i ++){
+    pieceQueueDivs[pieceQueueDivs.length-1][i].style.color = COLORS[type];
+    head.appendChild(pieceQueueDivs[pieceQueueDivs.length-1][i]);
+  }
+}
+
+function shiftPieceQueue(){
+  
+  for(let i = 0; i 
 }
 
 function drawBlock(x, y, type, boundingBox){
